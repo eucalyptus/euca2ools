c7ffb83008b72b0fc5b10287b350e44fcd32b787
Neil <neil@pall>
2009-04-14 10:09:19 -0700

    refactored for packaging

--- deps/M2Crypto-0.19.1/build/lib.linux-x86_64-2.5/M2Crypto/callback.py
+++
@@ -1,7 +0,0 @@
-"""Copyright (c) 1999-2003 Ng Pheng Siong. All rights reserved."""
-
-import warnings
-
-warnings.warn('Use the util module instead', DeprecationWarning)
-
-from util import genparam_callback, passphrase_callback
