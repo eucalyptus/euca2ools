6435df6040bf72ea15e8e3ab9abc5f7c8c3e4793
Garrett Holmstrom <gholms@fedoraproject.org>
2012-04-14 14:05:08 -0700

    Rewrite AssociateAddress

--- euca2ools/commands/euca/associateaddress.py
+++ euca2ools/commands/euca/associateaddress.py
@@ -1,6 +1,6 @@
 # Software License Agreement (BSD License)
 #
-# Copyright (c) 2009-2011, Eucalyptus Systems, Inc.
+# Copyright (c) 2009-2012, Eucalyptus Systems, Inc.
 # All rights reserved.
 #
 # Redistribution and use of this software in source and binary forms, with or
@@ -27,35 +27,17 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: Neil Soman neil@eucalyptus.com
-#         Mitch Garnaat mgarnaat@eucalyptus.com
 
-import euca2ools.commands.eucacommand
-from boto.roboto.param import Param
+from requestbuilder import Arg
+from . import EucalyptusRequest
 
-class AssociateAddress(euca2ools.commands.eucacommand.EucaCommand):
+class AssociateAddress(EucalyptusRequest):
+    Description = 'Associate an instance with a public IP address'
+    Args = [Arg('-i', '--instance', dest='InstanceId', metavar='INSTANCE',
+                required=True, help='instance to associate the address with'),
+            Arg('PublicIp', metavar='ADDRESS', help='IP address to associate')]
 
-    Description = 'Associate an instance with a public IP address.'
-    Options = [Param(name='instance_id', short_name='i', long_name='instance',
-                     optional=False, ptype='string',
-                     doc="""unique identifier for a running instance
-                     to associate the address with.""")]
-    Args = [Param(name='ip', ptype='string',
-                  doc='IP address to associate',
-                  cardinality=1, optional=False)]
-
-    def main(self):
-        conn = self.make_connection_cli()
-        return self.make_request_cli(conn, 'associate_address',
-                                     instance_id=self.instance_id,
-                                     public_ip=self.ip)
-
-    def main_cli(self):
-        status = self.main()
-        if status:
-            print 'ADDRESS\t%s\t%s' % (self.ip, self.instance_id)
-        else:
-            self.error_exit()
-
-
+    def print_result(self, result):
+        print self.tabify(('ADDRESS', self.args['PublicIp'],
+                           self.args['InstanceId'],
+                           result.get('associationId')))
