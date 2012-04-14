22dca6bde2cdffb4f5e956bca81491bedc68f395
Garrett Holmstrom <gholms@fedoraproject.org>
2012-04-14 13:49:54 -0700

    Rewrite CreateAccessKey

--- euca2ools/commands/euare/createaccesskey.py
+++ euca2ools/commands/euare/createaccesskey.py
@@ -1,6 +1,6 @@
 # Software License Agreement (BSD License)
 #
-# Copyright (c) 2009-2011, Eucalyptus Systems, Inc.
+# Copyright (c) 2009-2012, Eucalyptus Systems, Inc.
 # All rights reserved.
 #
 # Redistribution and use of this software in source and binary forms, with or
@@ -27,44 +27,16 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: Neil Soman neil@eucalyptus.com
-#         Mitch Garnaat mgarnaat@eucalyptus.com
 
-from boto.roboto.awsqueryrequest import AWSQueryRequest
-from boto.roboto.param import Param
-import euca2ools.commands.euare
-import euca2ools.utils
+from requestbuilder import Arg
+from . import EuareRequest, DELEGATE
 
+class CreateAccessKey(EuareRequest):
+    Description = 'Create a new access key for a user'
+    Args = [Arg('-u', '--user-name', dest='UserName',
+                help='user the new key will belong to (default: calling user'),
+            DELEGATE]
 
-class CreateAccessKey(AWSQueryRequest):
-
-    ServiceClass = euca2ools.commands.euare.Euare
-
-    Description = """CreateAccessKey"""
-    Params = [Param(
-        name='UserName',
-        short_name='u',
-        long_name='user-name',
-        ptype='string',
-        optional=True,
-        doc=""" The User name that the new key will belong to. """,
-        ), Param(
-        name='DelegateAccount',
-        short_name=None,
-        long_name='delegate',
-        ptype='string',
-        optional=True,
-        doc=""" [Eucalyptus extension] Process this command as if the administrator of the specified account had run it. This option is only usable by cloud administrators. """,
-        )]
-
-    def cli_formatter(self, data):
-        print data.AccessKey['AccessKeyId']
-        print data.AccessKey['SecretAccessKey']
-
-    def main(self, **args):
-       return self.send(**args)
-
-    def main_cli(self):
-        euca2ools.utils.print_version_if_necessary()
-        self.do_cli()
+    def print_result(self, result):
+        print result['AccessKey']['AccessKeyId']
+        print result['AccessKey']['SecretAccessKey']
