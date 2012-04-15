8936fbedceca1adebdbb3ee47695ad18bdcf4283
Garrett Holmstrom <gholms@fedoraproject.org>
2012-04-14 19:04:41 -0700

    Rewrite CreateSecurityGroup

--- euca2ools/commands/euca/createsecuritygroup.py
+++ euca2ools/commands/euca/createsecuritygroup.py
@@ -1,6 +1,6 @@
 # Software License Agreement (BSD License)
 #
-# Copyright (c) 2009-2011, Eucalyptus Systems, Inc.
+# Copyright (c) 2009-2012, Eucalyptus Systems, Inc.
 # All rights reserved.
 #
 # Redistribution and use of this software in source and binary forms, with or
@@ -27,35 +27,17 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: Neil Soman neil@eucalyptus.com
-#         Mitch Garnaat mgarnaat@eucalyptus.com
 
-import euca2ools.commands.eucacommand
-from boto.roboto.param import Param
+from requestbuilder import Arg
+from . import EucalyptusRequest
 
-class CreateSecurityGroup(euca2ools.commands.eucacommand.EucaCommand):
+class CreateSecurityGroup(EucalyptusRequest):
+    Description = 'Create a new security group'
+    Args = [Arg('-d', '--description', dest='GroupDescription', metavar='DESC',
+                required=True),
+            Arg('GroupName', metavar='GROUP', help='name of the new group')]
 
-    Description = 'Creates a new security group.'
-    Options = [Param(name='group_description', short_name='d',
-                     long_name='description',
-                     optional=False, ptype='string',
-                     doc='Description for the group to be created')]
-    Args = [Param(name='group_name', ptype='string',
-                  doc='unique name for the group to be created',
-                  cardinality=1, optional=False)]
-
-    def display_group(self, group):
-        print 'GROUP\t%s\t%s' % (group.name, group.description)
-
-    def main(self):
-        conn = self.make_connection_cli()
-        return self.make_request_cli(conn, 'create_security_group',
-                                     name=self.group_name,
-                                     description=self.group_description)
-
-    def main_cli(self):
-        group = self.main()
-        self.display_group(group)
-
-
+    def print_result(self, result):
+        print self.tabify(('GROUP', result.get('groupId'),
+                           self.args['GroupName'],
+                           self.args['GroupDescription']))
--- euca2ools/commands/euca/modgroup.py
+++ euca2ools/commands/euca/modgroup.py
@@ -28,9 +28,9 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-from . import EucalyptusRequest
 from requestbuilder import Arg, MutuallyExclusiveArgList
 import sys
+from . import EucalyptusRequest
 
 class ModifySecurityGroupRequest(EucalyptusRequest):
     '''
