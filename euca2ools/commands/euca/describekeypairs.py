acc480c1965fe7bdf25b2c4569049437c26f10d2
Garrett Holmstrom <gholms@fedoraproject.org>
2012-04-23 21:38:56 -0700

    Rewrite DescribeKeyPairs

--- euca2ools/commands/euca/describekeypairs.py
+++ euca2ools/commands/euca/describekeypairs.py
@@ -1,6 +1,6 @@
 # Software License Agreement (BSD License)
 #
-# Copyright (c) 2009-2011, Eucalyptus Systems, Inc.
+# Copyright (c) 2009-2012, Eucalyptus Systems, Inc.
 # All rights reserved.
 #
 # Redistribution and use of this software in source and binary forms, with or
@@ -27,35 +27,21 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: Neil Soman neil@eucalyptus.com
-#         Mitch Garnaat mgarnaat@eucalyptus.com
 
-import euca2ools.commands.eucacommand
-from boto.roboto.param import Param
+from requestbuilder import Arg, Filter
+from . import EucalyptusRequest
 
-class DescribeKeyPairs(euca2ools.commands.eucacommand.EucaCommand):
-
+class DescribeKeyPairs(EucalyptusRequest):
     APIVersion = '2010-08-31'
-    Description = 'Shows information about keypairs.'
-    Args = [Param(name='keypair', ptype='string',
-                  cardinality='+', optional=True)]
-    Filters = [Param(name='fingerprint', ptype='string',
-                     doc='Fingerprint of the key pair.'),
-               Param(name='key-name', ptype='string',
-                     doc='Name of the keypair.')]
-    
-    def display_keypairs(self, keypairs):
-        for keypair in keypairs:
-            keypair_string = '%s\t%s' % (keypair.name, keypair.fingerprint)
-            print 'KEYPAIR\t%s' % keypair_string
+    Description = 'Display information about available key pairs'
+    Args = [Arg('KeyName', nargs='*', metavar='KEYPAIR',
+                help='limit results to specific key pairs')]
+    Filters = [Filter('fingerprint', help='fingerprint of the key pair'),
+               Filter('key-name', help='name of the key pair')]
+    ListMarkers = ['keySet']
+    ItemMarkers = ['item']
 
-    def main(self):
-        conn = self.make_connection_cli()
-        return self.make_request_cli(conn, 'get_all_key_pairs',
-                                     keynames=self.keypair)
-
-    def main_cli(self):
-        keypairs = self.main()
-        self.display_keypairs(keypairs)
-
+    def print_result(self, result):
+        for key in result.get('keySet', []):
+            print self.tabify(('KEYPAIR', key.get('keyName'),
+                               key.get('keyFingerprint')))
