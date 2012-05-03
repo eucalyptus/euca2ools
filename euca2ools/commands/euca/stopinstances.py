d1c4d7fa652c7705b90f21de36e4b12589b27857
Garrett Holmstrom <gholms@fedoraproject.org>
2012-05-02 23:01:09 -0700

    Rewrite StopInstances

--- euca2ools/commands/euca/stopinstances.py
+++ euca2ools/commands/euca/stopinstances.py
@@ -1,6 +1,6 @@
 # Software License Agreement (BSD License)
 #
-# Copyright (c) 20092011, Eucalyptus Systems, Inc.
+# Copyright (c) 2009-2012, Eucalyptus Systems, Inc.
 # All rights reserved.
 #
 # Redistribution and use of this software in source and binary forms, with or
@@ -27,34 +27,22 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: Neil Soman neil@eucalyptus.com
-#         Mitch Garnaat mgarnaat@eucalyptus.com
 
-import euca2ools.commands.eucacommand
-from boto.roboto.param import Param
+from requestbuilder import Arg
+from . import EucalyptusRequest
 
-class StopInstances(euca2ools.commands.eucacommand.EucaCommand):
+class StopInstances(EucalyptusRequest):
+    Description = 'Stop one or more running instances'
+    Args = [Arg('InstanceId', metavar='INSTANCE', nargs='+',
+                help='instance(s) to stop'),
+            Arg('-f', '--force', dest='Force', action='store_const',
+                const='true',
+                help='immediately stop the instance. Data may be lost')]
+    ListMarkers = ['instancesSet']
+    ItemMarkers = ['item']
 
-    Description = 'Stops the specified EBS-based instances.'
-    Options = [Param(name='force', short_name='f', long_name='force',
-                    optional=True, ptype='boolean', default=False,
-                    doc='Forces the instance to stop.')]
-    Args = [Param(name='instance_id', ptype='string',
-                  optional=False, cardinality='+',
-                  doc='unique identifier for instance to stop')]
-
-    def display_instances(self, instances):
-        for instance in instances:
-            print 'INSTANCE\t%s' % instance.id
-            
-    def main(self):
-        conn = self.make_connection_cli()
-        return self.make_request_cli(conn, 'stop_instances',
-                                     instance_ids=self.instance_id,
-                                     force=self.force)
-
-    def main_cli(self):
-        instances = self.main()
-        self.display_instances(instances)
-
+    def print_result(self, result):
+        for instance in result.get('instancesSet', []):
+            print self.tabify(('INSTANCE', instance.get('instanceId'),
+                               instance.get('previousState', {}).get('name'),
+                               instance.get('currentState',  {}).get('name')))
