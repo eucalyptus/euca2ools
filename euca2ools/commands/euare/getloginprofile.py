fb2905906dc07696554f93b78a79c8196fd591e7
Garrett Holmstrom <gholms@fedoraproject.org>
2013-02-18 21:49:23 -0800

    Port GetLoginProfile

--- euca2ools/commands/euare/getloginprofile.py
+++ euca2ools/commands/euare/getloginprofile.py
@@ -1,6 +1,6 @@
 # Software License Agreement (BSD License)
 #
-# Copyright (c) 2009-2011, Eucalyptus Systems, Inc.
+# Copyright (c) 2009-2013, Eucalyptus Systems, Inc.
 # All rights reserved.
 #
 # Redistribution and use of this software in source and binary forms, with or
@@ -27,43 +27,28 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: Neil Soman neil@eucalyptus.com
-#         Mitch Garnaat mgarnaat@eucalyptus.com
 
-from boto.roboto.awsqueryrequest import AWSQueryRequest
-from boto.roboto.param import Param
-import euca2ools.commands.euare
-import euca2ools.utils
+from requestbuilder import Arg
+from . import EuareRequest, DELEGATE
 
 
-class GetLoginProfile(AWSQueryRequest):
+class GetLoginProfile(EuareRequest):
+    DESCRIPTION = 'Verify that a user has a password'
+    ARGS = [Arg('-u', '--user-name', dest='UserName', metavar='USER',
+                required=True, help='user whose password to verify (required)'),
+            Arg('--verbose', action='store_true', route_to=None,
+                help="print extra info about the user's password"),
+            DELEGATE]
 
-    ServiceClass = euca2ools.commands.euare.Euare
-
-    Description = """GetLoginProfile"""
-    Params = [Param(
-        name='UserName',
-        short_name='u',
-        long_name='user-name',
-        ptype='string',
-        optional=False,
-        doc=""" Name of the User whose login profile you want to retrieve. """ ,
-        ), Param(
-        name='DelegateAccount',
-        short_name=None,
-        long_name='delegate',
-        ptype='string',
-        optional=True,
-        doc=""" [Eucalyptus extension] Process this command as if the administrator of the specified account had run it. This option is only usable by cloud administrators. """,
-        )]
-
-    def cli_formatter(self, data):
-        print data.LoginProfile['UserName']
-
-    def main(self, **args):
-        return self.send(**args)
-
-    def main_cli(self):
-        euca2ools.utils.print_version_if_necessary()
-        self.do_cli()
+    def print_result(self, result):
+        # If we've managed to get to this point, we already know the user has
+        # a login profile.
+        user_name = result['LoginProfile'].get('UserName')
+        print 'Login Profile Exists for User', user_name
+        if self.args['verbose']:
+            create_date = result['LoginProfile'].get('CreateDate')
+            if create_date:
+                print 'Creation date:', create_date
+            must_change = result['LoginProfile'].get('MustChangePassword')
+            if must_change:
+                print 'Must change password:', must_change
