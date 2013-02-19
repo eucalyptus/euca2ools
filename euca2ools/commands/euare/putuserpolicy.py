05cca0845ae67da633b512224b909ea7c8a1f1e6
Garrett Holmstrom <gholms@fedoraproject.org>
2013-02-18 22:58:34 -0800

    Port PutUserPolicy

--- euca2ools/commands/euare/putuserpolicy.py
+++ euca2ools/commands/euare/putuserpolicy.py
@@ -1,6 +1,6 @@
 # Software License Agreement (BSD License)
 #
-# Copyright (c) 2009-2011, Eucalyptus Systems, Inc.
+# Copyright (c) 2009-2013, Eucalyptus Systems, Inc.
 # All rights reserved.
 #
 # Redistribution and use of this software in source and binary forms, with or
@@ -27,64 +27,21 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: Neil Soman neil@eucalyptus.com
-#         Mitch Garnaat mgarnaat@eucalyptus.com
 
-from boto.roboto.awsqueryrequest import AWSQueryRequest
-from boto.roboto.param import Param
-import euca2ools.commands.euare
-import euca2ools.utils
+from requestbuilder import Arg, MutuallyExclusiveArgList
+from . import EuareRequest, DELEGATE
 
 
-class PutUserPolicy(AWSQueryRequest):
-
-    ServiceClass = euca2ools.commands.euare.Euare
-
-    Description = """PutUserPolicy"""
-    Params = [Param(
-        name='UserName',
-        short_name='u',
-        long_name='user-name',
-        ptype='string',
-        optional=False,
-        doc=""" Name of the User to associate the policy with. """,
-        ), Param(
-        name='PolicyName',
-        short_name='p',
-        long_name='policy-name',
-        ptype='string',
-        optional=False,
-        doc=""" Name of the policy document. """,
-        ), Param(
-        name='PolicyDocument',
-        short_name='o',
-        long_name='policy-content',
-        ptype='string',
-        optional=True,
-        doc=""" The content of policy document. """,
-        ), Param(
-        name='PolicyDocument',
-        short_name='f',
-        long_name='policy-document',
-        ptype='file',
-        optional=True,
-        doc=""" The policy document as a file. """,
-        ), Param(
-        name='DelegateAccount',
-        short_name=None,
-        long_name='delegate',
-        ptype='string',
-        optional=True,
-        doc=""" [Eucalyptus extension] Process this command as if the administrator of the specified account had run it. This option is only usable by cloud administrators. """,
-        )]
-
-    def cli_formatter(self, data):
-        pass
-    
-    def main(self, **args):
-        return self.send(**args)
-
-    def main_cli(self):
-        euca2ools.utils.print_version_if_necessary()
-        self.do_cli()
+class PutUserPolicy(EuareRequest):
+    DESCRIPTION = 'Attach a policy to a user'
+    ARGS = [Arg('-u', '--user-name', dest='UserName', metavar='USER',
+                required=True, help='user to attach the policy to (required)'),
+            Arg('-p', '--policy-name', dest='PolicyName', metavar='POLICY',
+                required=True, help='name of the policy (required)'),
+            MutuallyExclusiveArgList(True,
+                Arg('-o', '--policy-content', dest='PolicyDocument',
+                    metavar='POLICY_CONTENT', help='the policy to attach'),
+                Arg('-f', '--policy-document', dest='PolicyDocument',
+                    metavar='FILE', type=open,
+                    help='file containing the policy to attach')),
+            DELEGATE]
