bc0ef35fc26a809b94b430efbe293ad1fefe897d
Garrett Holmstrom <gholms@fedoraproject.org>
2013-02-19 12:39:05 -0800

    Port UpdateUserInfo

--- euca2ools/commands/euare/updateuserinfo.py
+++ euca2ools/commands/euare/updateuserinfo.py
@@ -1,6 +1,6 @@
 # Software License Agreement (BSD License)
 #
-# Copyright (c) 2009-2011, Eucalyptus Systems, Inc.
+# Copyright (c) 2009-2013, Eucalyptus Systems, Inc.
 # All rights reserved.
 #
 # Redistribution and use of this software in source and binary forms, with or
@@ -27,57 +27,17 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-#
-# Author: Neil Soman neil@eucalyptus.com
-#         Mitch Garnaat mgarnaat@eucalyptus.com
 
-from boto.roboto.awsqueryrequest import AWSQueryRequest
-from boto.roboto.param import Param
-import euca2ools.commands.euare
-import euca2ools.utils
+from requestbuilder import Arg
+from . import EuareRequest, DELEGATE
 
 
-class UpdateUserInfo(AWSQueryRequest):
-
-    ServiceClass = euca2ools.commands.euare.Euare
-
-    Description = """UpdateUserInfo"""
-    Params = [Param(
-        name='UserName',
-        short_name='u',
-        long_name='user-name',
-        ptype='string',
-        optional=True,
-        doc=""" Name of the User. """,
-        ), Param(
-        name='InfoKey',
-        short_name='k',
-        long_name='info-key',
-        ptype='string',
-        optional=False,
-        doc=""" Specify the name of the user information to set. """,
-        ), Param(
-        name='InfoValue',
-        short_name='i',
-        long_name='info-value',
-        ptype='string',
-        optional=True,
-        doc=""" Specify the value of the user information to set, without setting this will effectively remove the specified information. """,
-        ), Param(
-        name='DelegateAccount',
-        short_name=None,
-        long_name='delegate',
-        ptype='string',
-        optional=True,
-        doc=""" [Eucalyptus extension] Process this command as if the administrator of the specified account had run it. This option is only usable by cloud administrators. """,
-        )]
-
-    def cli_formatter(self, data):
-        pass
-    
-    def main(self, **args):
-        return self.send(**args)
-
-    def main_cli(self):
-        euca2ools.utils.print_version_if_necessary()
-        self.do_cli()
+class UpdateUserInfo(EuareRequest):
+    DESCRIPTION = "[Eucalyptus only] Update a user's information"
+    ARGS = [Arg('-u', '--user-name', dest='UserName', metavar='USER',
+                help='user to update (default: current user)'),
+            Arg('-k', '--info-key', dest='InfoKey', metavar='KEY',
+                required=True, help='name of the info field to set (required)'),
+            Arg('-i', '--info-value', dest='InfoValue', metavar='VALUE',
+                help='value to set the info field to (omit to delete it)'),
+            DELEGATE]
