a6036b32aa1255920e4e9b5ed3075c6232031e11
Garrett Holmstrom <gholms@fedoraproject.org>
2013-02-28 17:13:02 -0800

    Implement DescribeAutoScalingGroups
    Fixes TOOLS-204

---
+++ euca2ools/commands/autoscaling/describeautoscalinggroups.py
@@ -0,0 +1,86 @@
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2013, Eucalyptus Systems, Inc.
+# All rights reserved.
+#
+# Redistribution and use of this software in source and binary forms, with or
+# without modification, are permitted provided that the following conditions
+# are met:
+#
+#   Redistributions of source code must retain the above
+#   copyright notice, this list of conditions and the
+#   following disclaimer.
+#
+#   Redistributions in binary form must reproduce the above
+#   copyright notice, this list of conditions and the
+#   following disclaimer in the documentation and/or other
+#   materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from euca2ools.commands.autoscaling import AutoScalingRequest
+from requestbuilder import Arg
+from requestbuilder.mixins import TabifyingCommand
+from requestbuilder.response import PaginatedResponse
+
+class DescribeAutoScalingGroups(AutoScalingRequest, TabifyingCommand):
+    DESCRIPTION = 'Describe auto-scaling groups'
+    ARGS = [Arg('AutoScalingGroupNames.member', metavar='ASGROUP',
+                nargs='*',
+                help='limit results to specific auto-scaling groups'),
+            Arg('--show-long', action='store_true', route_to=None,
+                help="show all of the groups' info")]
+    LIST_MARKERS = ['AutoScalingGroups', 'AvailabilityZones',
+                    'EnabledMetrics', 'Instances', 'LoadBalancerNames',
+                    'SuspendedProcesses', 'Tags', 'TerminationPolicies']
+
+    def main(self):
+        return PaginatedResponse(self, (None,), ('AutoScalingGroups',))
+
+    def prepare_for_page(self, page):
+        # Pages are defined by NextToken
+        self.params['NextToken'] = page
+
+    def get_next_page(self, response):
+        return response.get('NextToken') or None
+
+    def print_result(self, result):
+        for group in result.get('AutoScalingGroups', []):
+            bits = ['AUTO-SCALING-GROUP']
+            bits.append(group.get('AutoScalingGroupName'))
+            bits.append(group.get('LaunchConfigurationName'))
+            bits.append(','.join(group.get('AvailabilityZones')))
+            if self.args['show_long']:
+                bits.append(group.get('CreatedTime'))
+            balancers = group.get('LoadBalancerNames')
+            if balancers:
+                bits.append(','.join(balancers))
+            else:
+                bits.append(None)
+            if self.args['show_long']:
+                bits.append(group.get('HealthCheckType'))
+            bits.append(group.get('MinSize'))
+            bits.append(group.get('MaxSize'))
+            bits.append(group.get('DesiredCapacity'))
+            if self.args['show_long']:
+                bits.append(group.get('DefaultCooldown'))
+                bits.append(group.get('HealthCheckGracePeriod'))
+                bits.append(group.get('VPCZoneIdentifier'))
+                bits.append(group.get('PlacementGroup'))
+                bits.append(group.get('AutoScalingGroupARN'))
+            policies = group.get('TerminationPolicies')
+            if policies:
+                bits.append(','.join(policies))
+            else:
+                bits.append(None)
+            print self.tabify(bits)
