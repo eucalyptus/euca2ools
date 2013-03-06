75e5d4ad0cae986470d326452f68d376e2b3be86
Garrett Holmstrom <gholms@fedoraproject.org>
2013-03-05 16:50:21 -0800

    Implement DescribePolicies
    I'm a little concerned about ambiguous documentation with respect to how
    I'm supposed to display alarms, so for the time being, we aren't going
    to print them at all.  If you figure out what it's supposed to look
    like, patches are welcome!
    
    Fixes TOOLS-219

---
+++ euca2ools/commands/autoscaling/describepolicies.py
@@ -0,0 +1,69 @@
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2013, Eucalyptus Systems, Inc.
+# All rights reserved.
+#
+# Redistribution and use of this software in source and binary forms, with or
+# without modification, are permitted provided that the following conditions
+# are met:
+#
+#   Redistributions of source code must retain the above
+#   copyright notice, this list of conditions and the
+#   following disclaimer.
+#
+#   Redistributions in binary form must reproduce the above
+#   copyright notice, this list of conditions and the
+#   following disclaimer in the documentation and/or other
+#   materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from euca2ools.commands.autoscaling import AutoScalingRequest
+from requestbuilder import Arg
+from requestbuilder.mixins import TabifyingCommand
+from requestbuilder.response import PaginatedResponse
+
+
+class DescribePolicies(AutoScalingRequest, TabifyingCommand):
+    DESCRIPTION = 'Describe auto-scaling policies'
+    ARGS = [Arg('PolicyNames.member', metavar='POLICY', nargs='*',
+                help='limit results to specific auto-scaling policies'),
+            Arg('-g', '--auto-scaling-group', dest='AutoScalingGroupName',
+                metavar='ASGROUP'),
+            Arg('--show-long', action='store_true', route_to=None,
+                help="show all of the policies' info")]
+    LIST_MARKERS = ['ScalingPolicies', 'Alarms']
+
+    def main(self):
+        return PaginatedResponse(self, (None,), ('ScalingPolicies',))
+
+    def prepare_for_page(self, page):
+        # Pages are defined by NextToken
+        self.params['NextToken'] = page
+
+    def get_next_page(self, response):
+        return response.get('NextToken') or None
+
+    def print_result(self, result):
+        for policy in result.get('ScalingPolicies', []):
+            bits = ['SCALING-POLICY']
+            bits.append(policy.get('AutoScalingGroupName'))
+            bits.append(policy.get('PolicyName'))
+            bits.append(policy.get('ScalingAdjustment'))
+            if self.args['show_long']:
+                bits.append(policy.get('MinAdjustmentStep'))
+            bits.append(policy.get('AdjustmentType'))
+            if self.args['show_long']:
+                bits.append(policy.get('Cooldown'))
+            bits.append(policy.get('PolicyARN'))
+            print self.tabify(bits)
