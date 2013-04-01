1c8fe50205a8c0360121980f5fb65ff37e5b05ab
Garrett Holmstrom <gholms@fedoraproject.org>
2013-03-31 18:05:11 -0700

    Implement CreateAppCookieStickinessPolicy
    Fixes TOOLS-247

---
+++ euca2ools/commands/elasticloadbalancing/createappcookiestickinesspolicy.py
@@ -0,0 +1,48 @@
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2013, Eucalyptus Systems, Inc.
+# All rights reserved.
+#
+# Redistribution and use of this software in source and binary forms, with or
+# without modification, are permitted provided that the following conditions
+# are met:
+#
+#   Redistributions of source code must retain the above
+#   copyright notice, this list of conditions and the
+#   following disclaimer.
+#
+#   Redistributions in binary form must reproduce the above
+#   copyright notice, this list of conditions and the
+#   following disclaimer in the documentation and/or other
+#   materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+from euca2ools.commands.elasticloadbalancing import ELBRequest
+from requestbuilder import Arg
+
+
+class CreateAppCookieStickinessPolicy(ELBRequest):
+    DESCRIPTION = ('Create a new stickiness policy for a load balancer, '
+                   'whereby the server application generates a cookie and '
+                   'adds it to its responses.  The load balancer will then '
+                   'use this cookie to route requests to the same back end '
+                   'instance cookie.  This type of policy can only be '
+                   'associated with HTTP or HTTPS listeners')
+    ARGS = [Arg('LoadBalancerName', metavar='ELB',
+                help='name of the load balancer to modify (required)'),
+            # -c is for cookie.  That's good enough for me.
+            Arg('-c', '--cookie-name', dest='CookieName', required=True,
+                help='name of the cookie used for stickiness (required)'),
+            Arg('-p', '--policy-name', dest='PolicyName', metavar='POLICY',
+                required=True, help='name of the new policy (required)')]
