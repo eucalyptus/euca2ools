05b217ff021a67a3b193b2da66ab1c253d3effdf
Garrett Holmstrom <gholms@devzero.com>
2014-03-20 18:22:50 -0700

    Use euare-roleaddpolicy's multi-action/resource policy generator for user/group commands

--- euca2ools/commands/euare/addgrouppolicy.py
+++ euca2ools/commands/euare/addgrouppolicy.py
@@ -1,4 +1,4 @@
-# Copyright 2009-2013 Eucalyptus Systems, Inc.
+# Copyright 2009-2014 Eucalyptus Systems, Inc.
 #
 # Redistribution and use of this software in source and binary forms,
 # with or without modification, are permitted provided that the following
@@ -23,11 +23,13 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-import datetime
+import json
+
+from requestbuilder import Arg
+
 from euca2ools.commands.euare import EuareRequest, AS_ACCOUNT
 from euca2ools.commands.euare.putgrouppolicy import PutGroupPolicy
-import json
-from requestbuilder import Arg
+from euca2ools.util import build_iam_policy
 
 
 class AddGroupPolicy(EuareRequest):
@@ -39,23 +41,20 @@ class AddGroupPolicy(EuareRequest):
                 help='name of the new policy (required)'),
             Arg('-e', '--effect', choices=('Allow', 'Deny'), required=True,
                 help='whether the new policy should Allow or Deny (required)'),
-            Arg('-a', '--action', required=True,
-                help='action the policy should apply to (required)'),
-            Arg('-r', '--resource', required=True,
-                help='resource the policy should apply to (required)'),
+            Arg('-a', '--action', dest='actions', action='append',
+                required=True, help='''action(s) the policy should apply to
+                (at least one required)'''),
+            Arg('-r', '--resource', dest='resources', action='append',
+                required=True, help='''resource(s) the policy should apply to
+                (at least one required)'''),
             Arg('-o', '--output', action='store_true',
                 help='display the newly-created policy'),
             AS_ACCOUNT]
 
-    def build_policy(self):
-        stmt = {'Sid': datetime.datetime.utcnow().strftime('%Y%m%d%H%M%S%f'),
-                'Effect':   self.args['effect'],
-                'Action':   self.args['action'],
-                'Resource': self.args['resource']}
-        return {'Statement': [stmt]}
-
     def main(self):
-        policy_doc = json.dumps(self.build_policy())
+        policy = build_iam_policy(self.args['effect'], self.args['resources'],
+                                  self.args['actions'])
+        policy_doc = json.dumps(policy)
         req = PutGroupPolicy(
             config=self.config, service=self.service,
             GroupName=self.args['group_name'],
