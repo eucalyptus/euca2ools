884b260887b5db82084f4dcfe22947403cc257f7
Garrett Holmstrom <gholms@devzero.com>
2014-03-20 17:15:36 -0700

    Implement GetRole
    Implements TOOLS-435

---
+++ euca2ools/commands/euare/getrole.py
@@ -0,0 +1,44 @@
+# Copyright 2014 Eucalyptus Systems, Inc.
+#
+# Redistribution and use of this software in source and binary forms,
+# with or without modification, are permitted provided that the following
+# conditions are met:
+#
+#   Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+#   Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import urllib
+
+from requestbuilder import Arg
+
+from euca2ools.commands.euare import EuareRequest, AS_ACCOUNT
+
+
+class GetRole(EuareRequest):
+    DESCRIPTION = "Display a role's ARN, GUID, and policy"
+    ARGS = [Arg('-r', '--role-name', dest='RoleName', metavar='ROLE',
+                required=True,
+                help='name of the role to show info about (required)'),
+            AS_ACCOUNT]
+
+    def print_result(self, result):
+        print result.get('Role', {}).get('Arn')
+        print result.get('Role', {}).get('RoleId')
+        print urllib.unquote(result.get('Role', {})
+                             .get('AssumeRolePolicyDocument'))
