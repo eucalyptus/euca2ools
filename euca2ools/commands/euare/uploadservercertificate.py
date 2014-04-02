2a6068703a8460ec1e1b7bcf0d8ed1dbc81e0682
Garrett Holmstrom <gholms@devzero.com>
2014-04-01 17:28:34 -0700

    Fix euare-servercertupload --cert-chain-file type
    Fixes TOOLS-410

--- euca2ools/commands/euare/uploadservercertificate.py
+++ euca2ools/commands/euare/uploadservercertificate.py
@@ -50,9 +50,10 @@ class UploadServerCertificate(EuareRequest):
                     is typically the PEM-encoded certificates of the chain,
                     concatenated together.'''),
                 Arg('--certificate-chain-file', dest='CertificateChain',
-                    metavar='FILE', help='''file containing the PEM-encoded
-                    certificate chain. This is typically the PEM-encoded
-                    certificates of the chain, concatenated together.''')),
+                    type=open, metavar='FILE', help='''file containing the
+                    PEM-encoded certificate chain.  This is typically the
+                    PEM-encoded certificates of the chain, concatenated
+                    together.''')),
             Arg('-p', '--path', dest='Path',
                 help='path for the new server certificate (default: "/")'),
             AS_ACCOUNT]
