6286816146df2b0fe03f138dc497a90b8af1e614
Vic Iglesias <viglesiasce@gmail.com>
2014-04-07 23:06:10 -0700

    Fix copyright year.

--- euca2ools/commands/cloudformation/describestackresources.py
+++ euca2ools/commands/cloudformation/describestackresources.py
@@ -1,4 +1,4 @@
-# Copyright 2013 Eucalyptus Systems, Inc.
+# Copyright 2014 Eucalyptus Systems, Inc.
 #
 # Redistribution and use of this software in source and binary forms,
 # with or without modification, are permitted provided that the following
