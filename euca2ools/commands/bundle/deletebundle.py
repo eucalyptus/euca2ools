b792b835c9537a34f60c09c1a4c0f3566cd02236
Garrett Holmstrom <gholms@devzero.com>
2014-04-17 17:04:31 -0700

    DeleteBundle:  delete the bucket even with no manifest
    Fixes TOOLS-379

--- euca2ools/commands/bundle/deletebundle.py
+++ euca2ools/commands/bundle/deletebundle.py
@@ -24,6 +24,7 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from requestbuilder import Arg
+from requestbuilder.exceptions import ServerError
 
 from euca2ools.commands.bundle.mixins import BundleDownloadingMixin
 from euca2ools.commands.s3 import S3Request
@@ -37,21 +38,40 @@ class DeleteBundle(S3Request, BundleDownloadingMixin):
                 help='attempt to delete the bucket as well')]
 
     def main(self):
-        manifest = self.fetch_manifest(self.service)
-        for part, part_s3path in self.map_bundle_parts_to_s3paths(manifest):
-            req = DeleteObject(config=self.config, service=self.service,
-                               path=part_s3path)
+        try:
+            manifest = self.fetch_manifest(self.service)
+        except ServerError as err:
+            if err.status_code == 404 and self.args.get('clear'):
+                try:
+                    # We are supposed to try to delete the bucket even
+                    # if the manifest isn't there.  If it works, the
+                    # bundle is also gone and we can safely return.
+                    #
+                    # https://eucalyptus.atlassian.net/browse/TOOLS-379
+                    self.__delete_bucket()
+                    return
+                except ServerError:
+                    # If the bucket wasn't empty then we'll go back to
+                    # complaining about the missing manifest.
+                    self.log.error(
+                        'failed to delete bucket %s after a failed '
+                        'attempt to fetch the bundle manifest',
+                        bucket=self.args['bucket'].split('/')[0],
+                        exc_info=True)
+            raise
+
+        for _, part_s3path in self.map_bundle_parts_to_s3paths(manifest):
+            req = DeleteObject.from_other(self, path=part_s3path)
             req.main()
-            print '>>>', part, part_s3path
         manifest_s3path = self.get_manifest_s3path()
         if manifest_s3path:
-            req = DeleteObject(config=self.config, service=self.service,
-                               path=manifest_s3path)
+            req = DeleteObject.from_other(self, path=manifest_s3path)
             req.main()
-            print '>>> manifest', manifest_s3path
 
         if self.args.get('clear'):
-            req = DeleteBucket(service=self.service, config=self.config,
-                               bucket=self.args['bucket'].split('/')[0])
-            req.main()
-            print '>>> bucket', self.args['bucket'].split('/')[0]
+            self.__delete_bucket()
+
+    def __delete_bucket(self):
+        req = DeleteBucket.from_other(self,
+                                      bucket=self.args['bucket'].split('/')[0])
+        req.main()
