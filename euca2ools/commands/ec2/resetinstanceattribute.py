70a0a597a89cb1418904680d5530badac9c0357d
Garrett Holmstrom <gholms@devzero.com>
2014-05-07 16:05:52 -0700

    Implement ResetInstanceAttribute
    Fixes TOOLS-451

---
+++ euca2ools/commands/ec2/resetinstanceattribute.py
@@ -0,0 +1,45 @@
+# Copyright 2014 Eucalyptus Systems, Inc.
+#
+# Redistribution and use of this software in source and binary forms,
+# with or without modification, are permitted provided that the following
+# conditions are met:
+#
+#   Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+#   Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from requestbuilder import Arg, MutuallyExclusiveArgList
+
+from euca2ools.commands.ec2 import EC2Request
+
+
+class ResetInstanceAttribute(EC2Request):
+    DESCRIPTION = 'Reset an attribute of an instance to its default value'
+    ARGS = [Arg('InstanceId', metavar='INSTANCE',
+                help='ID of the instance to modify (required)'),
+            MutuallyExclusiveArgList(
+                Arg('--kernel', dest='Attribute', action='store_const',
+                    const='kernel',
+                    help="reset the instance's kernel image ID"),
+                Arg('--ramdisk', dest='Attribute', action='store_const',
+                    const='ramdisk',
+                    help="reset the instance's ramdisk image ID"),
+                Arg('--source-dest-check', dest='Attribute',
+                    action='store_const', const='sourceDestCheck',
+                    help="reset the instance's SourceDestCheck to true"))
+            .required()]
