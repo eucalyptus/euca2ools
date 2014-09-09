e531c5e6afa9fe0fa975bc7212f51ccad560d866
Garrett Holmstrom <gholms@devzero.com>
2014-09-09 15:06:44 -0700

    Update man pages

--- generate-manpages.sh
+++ generate-manpages.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -ex
+#!/bin/sh -e
 
 mkdir -p man
 export PYTHONPATH=".:$PYTHONPATH"
@@ -8,6 +8,7 @@ script_dir="build/scripts-$py_version"
 version="$($script_dir/euca-version 2>&1 | sed -e 's/^euca2ools *\([^(]*\).*/\1/' -e 's/ *$//')"
 
 for exe in $@; do
+    echo $exe
     description="$($script_dir/$exe --help 2>&1 | python -c 'import sys; print sys.stdin.read().split("\n\n")[1]')"
     help2man -N --no-discard-stderr -S "euca2ools $version" -n "$description" --version-string "$version" -o man/$(basename $exe).1 $script_dir/$exe
     sed -i -e 's/^.SH DESCRIPTION/.SH SYNOPSIS/' \
