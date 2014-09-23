c7c398c88e2a0d954fbad74e1eb695d9136c86a5
Garrett Holmstrom <gholms@devzero.com>
2014-09-23 13:48:56 -0700

    Implement Attach/DetachNetworkInterface

---
+++ euca2ools/commands/ec2/attachnetworkinterface.py
@@ -0,0 +1,43 @@
+# Copyright 2014 Eucalyptus Systems, Inc.
+#
+# Redistribution and use of this software in source and binary forms,
+# with or without modification, are permitted provided that the following
+# conditions are met:
+#
+#   Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+#   Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from requestbuilder import Arg
+
+from euca2ools.commands.ec2 import EC2Request
+
+
+class AttachNetworkInterface(EC2Request):
+    DESCRIPTION = 'Attach a VPC network interface to an instance'
+    ARGS = [Arg('-i', '--instance', dest='InstanceId', metavar='INSTANCE',
+                required=True,
+                help='instance to attach the network interface to (required)'),
+            Arg('-d', '--device-index', dest='DeviceIndex', metavar='INT',
+                type=int, required=True, help='''device index to attach the
+                network interface with (required)'''),
+            Arg('NetworkInterfaceId', metavar='INTERFACE',
+                help='network interface to attach to the instance (required)')]
+
+    def print_result(self, result):
+        print result.get('attachmentId')
