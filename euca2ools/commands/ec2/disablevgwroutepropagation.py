124e25db7b5b0ea2b185b70a058a377afda76f5d
Garrett Holmstrom <gholms@devzero.com>
2014-09-25 16:24:07 -0700

    Implement Enable/DisableVgwRoutePropagation

---
+++ euca2ools/commands/ec2/disablevgwroutepropagation.py
@@ -0,0 +1,39 @@
+# Copyright 2014 Eucalyptus Systems, Inc.
+#
+# Redistribution and use of this software in source and binary forms,
+# with or without modification, are permitted provided that the following
+# conditions are met:
+#
+#   Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+#   Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from requestbuilder import Arg
+
+from euca2ools.commands.ec2 import EC2Request
+
+
+class DisableVgwRoutePropagation(EC2Request):
+    DESCRIPTION = ('Make a VPC route table stop obtaining routes from a '
+                   'virtual private gateway')
+    ARGS = [Arg('--route-table', dest='RouteTableId', metavar='RTABLE',
+                required=True, help='''ID of the route table to stop
+                propagating routes to (required)'''),
+            Arg('--vgw', dest='GatewayId', metavar='VGATEWAY', required=True,
+                help='''ID of the virtual private gateway to stop
+                obtaining routes from (required)''')]
