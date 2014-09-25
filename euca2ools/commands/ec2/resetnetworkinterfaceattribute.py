eed535e958a19bbaa428546e0f7eeb73a59619a7
Garrett Holmstrom <gholms@devzero.com>
2014-09-25 16:15:20 -0700

    Implement ResetNetworkInterfaceAttribute

---
+++ euca2ools/commands/ec2/resetnetworkinterfaceattribute.py
@@ -0,0 +1,41 @@
+# Copyright 2014 Eucalyptus Systems, Inc.
+#
+# Redistribution and use of this software in source and binary forms,
+# with or without modification, are permitted provided that the following
+# conditions are met:
+#
+#   Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+#
+#   Redistributions in binary form must reproduce the above copyright
+#   notice, this list of conditions and the following disclaimer in the
+#   documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+from requestbuilder import Arg, MutuallyExclusiveArgList
+
+from euca2ools.commands.ec2 import EC2Request
+
+
+class ResetNetworkInterfaceAttribute(EC2Request):
+    DESCRIPTION = 'Reset an attribute of a VPC network interface'
+    ARGS = [Arg('NetworkInterfaceId', metavar='INTERFACE',
+                help='ID of the network interface to reset (required)'),
+            Arg('--source-dest-check', dest='Attribute', required=True,
+                action='store_const', const='sourceDestCheck',
+                help='enable source/destination checking')]
+
+    def print_result(self, result):
+        print self.tabify((self.params['Attribute'],
+                           self.params['NetworkInterfaceId'], 'RESET'))
