f4b8d6f0d15f6fab2fa5a098c5fa65808f41c2ba
Garrett Holmstrom <gholms@devzero.com>
2016-03-22 13:25:30 -0700

    Fix version number in SCM (3.2-devel)

--- euca2ools/commands/__init__.py
+++ euca2ools/commands/__init__.py
@@ -1,4 +1,4 @@
-# Copyright 2009-2014 Eucalyptus Systems, Inc.
+# Copyright (c) 2009-2016 Hewlett Packard Enterprise Development LP
 #
 # Redistribution and use of this software in source and binary forms,
 # with or without modification, are permitted provided that the following
